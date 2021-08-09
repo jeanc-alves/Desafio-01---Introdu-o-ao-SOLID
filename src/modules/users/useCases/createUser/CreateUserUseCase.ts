51f4744fe976d87486bdf539496edbf118d21daf
Jean Alves <jean.alves@inovan.do>
2021-08-09 01:35:19 -0300

    chore(desafio): first test

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -7,10 +7,10 @@ interface IRequest {
 }
 
 class CreateUserUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    return this.usersRepository.create({ name, email });
   }
 }
 
